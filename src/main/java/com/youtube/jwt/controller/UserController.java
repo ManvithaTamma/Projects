88ea7c1636ae9cd47e90f6b0e2f0d590011dccee
manvi <manvithatamma88@gmail.com>
2023-09-09 17:14:51 -0500

    pushing user class and entity

---
+++ src/main/java/com/youtube/jwt/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.youtube.jwt.controller;
+
+import com.youtube.jwt.entity.User;
+import com.youtube.jwt.service.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping({"/registerNewUser"})
+    public User registerNewUser(@RequestBody User user){
+        return userService.registerNewUser(user);
+    }
+
+    @PostConstruct
+    public void initRolesAndUsers(){
+        userService.initRolesAndUser();
+    }
+}
